0488b5af8ee13a56f201dac7f03de06eea139f66
Antriksh2000 <antrikshmahajan5@gmail.com>
2023-10-15 20:16:42 +0530

    hotstar frontend with list

---
+++ src/pages/list.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import Cards from "../components/Cards";
+import axios from "axios";
+
+export default function list() {
+  const [CardImages, setCardImages] = useState([]);
+  
+    useEffect(() => {
+    axios({
+      url: "http://localhost:3001/GetMyFavouriteCards",
+      method: "GET",
+      headers: {  
+      token: sessionStorage.getItem("token"),
+    },
+    })
+      .then((response) => {
+        setCardImages(response.data.data);
+      })
+      .catch((error) => {
+        console.error("error while fetching cardImages", error);
+      });
+  }, []);
+
+  return (
+    <div className="bg-gray-900 h-max text-white">
+      <Navbar />
+      {/* <MainSlider /> */}
+      <Cards name="Drama Shows" CardImages={CardImages} />
+    </div>
+  );
+}
